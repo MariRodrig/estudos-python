5b493e651bb0281534b6d1cb9264361c035645d0
Mariana Rodrigues <marianar.adm@outlook.com>
2025-01-13 22:22:08 -0300

    feature: adicionei estrutura de dados

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ oi eu sou a Mariana
 ##### Oi eu sou a Mariana
 
 E eu estou aprendendo Git
+
+Dia 13: adicionei estrutura de dados ao repositório
---
+++ tipos de variaveis.py
@@ -0,0 +1,13 @@
+
+idade = 31 # (Int) para numeros inteiro
+peso = 80.4 # (float) para numeros decimais
+nome = 'Mariana' # (str) para caracteres
+ativo = True # (bool) para valores logicos
+resultado = None # (none) para ausencia de valores
+
+# Como validar qual variável está sendo utilizada
+print(type(idade))
+print(type(peso))
+print(type(nome))
+
+print('Olá meu nome é', nome, 'e eu tenho', idade, 'anos de idade.')
